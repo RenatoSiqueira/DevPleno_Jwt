81f9cd7a63def1bb88d22a8bf10260ff6921c715
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-12-19 18:37:16 -0300

    Estudo de Jwt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  async componentDidMount() {
+    let token = localStorage.getItem('token')
+    if (!token) {
+      const login = await axios.post('http://localhost:3001/users/login', {
+        "email": "tuliofaria@devpleno.com",
+        "passwd": "123456"
+      })
+      token = login.data.token
+      localStorage.setItem('token', token)
+    }
+    const decoded = jwtDecode(token)
+    console.log(decoded)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
